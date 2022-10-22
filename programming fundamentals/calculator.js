727cc97188cdc7b90d06bf75c8d5fec35f435f7b
furman3547 <113787227+furman3547@users.noreply.github.com>
2022-10-22 12:42:37 -0500

    simple programs

---
+++ programming fundamentals/calculator.js
@@ -0,0 +1,35 @@
+function addnumbers(a,b){
+    let c = a + b;
+console.log(c)
+}
+function simplecalculator(a,b,o){
+    if(o =='+'){
+        let c = a + b 
+        console.log(c);
+
+    }
+    else if(o== '-'){
+        let c = a-b;
+        console.log(c);
+    }
+    
+    else if(o== '*'){
+        let c= a*b;
+        console.log(c)
+    }
+    else if(o=='/'){
+        let c=a/b;
+        console.log(c);
+
+    }else{
+        console.log("I Don't know what I am doing")
+        
+    }
+}
+
+
+simplecalculator(4,5,'+')
+simplecalculator(25,5,'-')
+simplecalculator(4,5,'*')
+simplecalculator(15,5,'/')
+simplecalculator(15,5,"$")
---
+++ programming fundamentals/myfirstprogram.js
@@ -0,0 +1,5 @@
+let age = 20;
+var myname = "DigitalCrafts"
+let newname = myname;
+console.log(newname);
+console.log(age);
